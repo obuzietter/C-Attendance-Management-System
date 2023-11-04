8e6f34d5a32d206a3014ed1ba347bd4a8dcac712
obuzietter <obuyacalvince672@gmail.com>
2023-11-04 23:18:06 +0300

    first commit

---
+++ src/main/java/com/mdc/attendance/management/system/ATTENDANCEMANAGEMENTSYSTEM.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mdc.attendance.management.system;
+
+/**
+ *
+ * @author OBUZIETTER
+ */
+public class ATTENDANCEMANAGEMENTSYSTEM {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
